dde90fcc953e50fe06cb413bb09abbbfd6c665cb
João Moura <joaomdmoura@gmail.com>
2020-09-30 21:55:18 -0300

    First version

---
+++ index.js
@@ -0,0 +1,37 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const githubToken = core.getInput('GITHUB_TOKEN');
+    const templatePath = core.getInput('PR_TEMPLATE_PATH');
+    const octokit = github.getOctokit(githubToken);
+    const description = github.context.payload.pull_request.body;
+    const prTemplate = (await octokit.repos.getContent({
+      owner: github.context.payload.pull_request.base.repo.owner.login,
+      repo: github.context.payload.pull_request.base.repo.name,
+      path: templatePath,
+    })).data.content;
+    
+    const templateContent = new Buffer.from(prTemplate, 'base64');
+
+    console.log(description)
+    console.log(templateContent)
+    console.log(templateContent == description)
+    
+    // let requiredReviews = [];
+    // const team = JSON.parse(core.getInput('team'));
+    // const githubToken = core.getInput('GITHUB_TOKEN');
+    
+    // team.forEach(member => {
+    //   requiredReviews.push(reviews.includes(member));
+    // });
+
+    // if(!requiredReviews.includes(true)) {
+    //   core.setFailed(`Expect at leat one review from the team: ${JSON.stringify(team)}`);
+    // }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+run();
